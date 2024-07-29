1791cf088b7382a0a43ba8cb5b8aae2affd60a93
Lucas Campanelli <lucas.campanelli@outlook.com>
2024-07-29 03:10:34 -0300

    Ativando o Service Worker

---
+++ service-worker.js
@@ -0,0 +1,75 @@
+'use-strict';
+
+
+// Nome do cache que será guardado
+const CACHE_NAME = "brinquedo-app-estatico";
+
+// Arquivos que serão armazenados em cache
+const FILES_TO_CACHE = [
+
+    'css/bootstrap.min.css',
+    'css/styles.css',
+    'icons/bx-child.svg',
+    'imgs/favicon.ico',
+    'imgs/logo.webp',
+    'js/app.js',
+    'js/bootstrap.bundle.min.js',
+    'offline.html'
+
+];
+
+
+// Instalação do Service Worker
+
+// Adicionando função ao evento "install" do componente que roda este script
+self.addEventListener('install', (evento) => {
+
+    console.log("Service Worker em instalação");
+
+
+    evento.waitUntil(
+
+        caches.open(CACHE_NAME).then((cache) => {
+
+            console.log("Service Worker está adicionando o cache estático");
+
+            return cache.addAll(FILES_TO_CACHE);
+
+        })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+
+
+// Ativando o Service Worker
+
+// Adicionando uma função ao evento 'Activate' do componente que está rodando este script
+self.addEventListener('activate', (evt) => {
+
+    console.log("Service Worker em ativação");
+
+    evt.waitUntil(
+
+        caches.keys().then(keyList => {
+
+            return Promise.all(keyList.map((key) => {
+
+                if(key !== CACHE_NAME) {
+
+                    return caches.delete(key);
+
+                }
+
+            }))
+
+        })
+
+    )
+
+    self.clients.claim();
+
+})
